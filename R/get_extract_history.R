b3d2705aa460c4281ac99e3df96d5d91e6d2c935
your name <eli.pousson@gmail.com>
2024-04-02 00:17:01 -0400

    Expand helper extract history functions

--- R/get_extract_history.R
+++ R/get_extract_history.R
@@ -1,7 +1,12 @@
-#' Get NHGIS Extract history
+#' Get IPUMS Extract histories
 #'
-#' @inheritDotParams ipumsr::get_extract_history
+#' @inheritDotParams ipumsr::get_extract_history -collection
 #' @keywords internal
+#' @name ipums_extract_history
+NULL
+
+#' @rdname ipums_extract_history
+#' @name get_nhgis_extract_history
 #' @export
 get_nhgis_extract_history <- function(...) {
   check_installed("withr")
@@ -13,3 +18,46 @@ get_nhgis_extract_history <- function(...) {
     }
   )
 }
+
+#' @rdname ipums_extract_history
+#' @name get_usa_extract_history
+#' @export
+get_usa_extract_history <- function(...) {
+  check_installed("withr")
+
+  withr::with_envvar(
+    c("IPUMS_DEFAULT_COLLECTION" = "usa"),
+    {
+      ipumsr::get_extract_history(...)
+    }
+  )
+}
+
+#' @rdname ipums_extract_history
+#' @name get_cps_extract_history
+#' @export
+get_cps_extract_history <- function(...) {
+  check_installed("withr")
+
+  withr::with_envvar(
+    c("IPUMS_DEFAULT_COLLECTION" = "cps"),
+    {
+      ipumsr::get_extract_history(...)
+    }
+  )
+}
+
+#' @rdname ipums_extract_history
+#' @name get_ipumsi_extract_history
+#' @export
+get_ipumsi_extract_history <- function(...) {
+  check_installed("withr")
+
+  withr::with_envvar(
+    c("IPUMS_DEFAULT_COLLECTION" = "ipumsi"),
+    {
+      ipumsr::get_extract_history(...)
+    }
+  )
+}
+
