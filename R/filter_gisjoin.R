2dc5113b9c17d81435af8d580094d889e897aef3
your name <eli.pousson@gmail.com>
2024-05-29 11:41:29 -0400

    Rename filter_gisjoin to filter_nhgisjoin

--- R/filter_gisjoin.R
+++ R/filter_gisjoin.R
@@ -5,22 +5,24 @@
 #' @param values Optional vector of GISJOIN values. state_fips and county_fips
 #'   are ignored if this is supplied.
 #' @keywords internal
-filter_gisjoin <- function(data,
+filter_nhgisjoin <- function(data,
                            state = NULL,
                            county = NULL,
                            values = NULL,
-                           gisjoin_col = "GISJOIN") {
+                           gisjoin_col = "NHGISJOIN") {
   if (is.null(values)) {
     check_installed("tigris")
 
     if (!is.null(state) && is.null(county)) {
-      state <- tigris:::validate_state(state = state, multiple = TRUE)
-      values <- paste0("G", state, "0")
+      state <- tigris::validate_state(state = state, multiple = TRUE)
+      pattern <- paste0(paste0("^G", state, "0"), collapse = "|")
     } else {
-      state <- tigris:::validate_state(state = state)
-      county <- tigris:::validate_county(state = state, county = county, multiple = TRUE)
-      values <- paste0("G", state, "0", county, "0")
+      state <- tigris::validate_state(state = state)
+      county <- tigris::validate_county(state = state, county = county, multiple = TRUE)
+      pattern <- paste0(paste0("^G", state, "0", county, "0"), collapse = "|")
     }
+
+    return(dplyr::filter(data, stringr::str_detect(.data[[gisjoin_col]], pattern)))
   }
 
   dplyr::filter(data, .data[[gisjoin_col]] %in% values)
