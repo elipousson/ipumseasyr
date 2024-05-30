f529d43fde38218c30700aeab1909417c73420aa
your name <eli.pousson@gmail.com>
2024-05-29 23:11:07 -0400

    Update usa_states

--- data-raw/usa_states.R
+++ data-raw/usa_states.R
@@ -1,8 +1,8 @@
 ## code to prepare `divisions` dataset goes here
 usa_states <- data.frame(
-  "STATE" = c(state.name, "District of Columbia")
+  "STATE" = c(state.name, "District of Columbia"),
   "STUSPS" = c(state.abb, "DC"),
-  "division" = factor(c(state.division, "South Atlantic"), levels = levels(state.division))
+  "division" = factor(c(state.division, "South Atlantic"), levels = levels(state.division)),
   "region" = factor(c(state.region, "South"), levels = levels(state.region))
 )
 
