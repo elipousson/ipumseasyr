f755f517dec82391c96d087d7ffc817a70e711c4
Eli Pousson <eli.pousson@gmail.com>
2025-04-02 15:14:38 -0400

    Fix issue w/ usa_states object

--- data-raw/usa_states.R
+++ data-raw/usa_states.R
@@ -2,8 +2,14 @@
 usa_states <- data.frame(
   "STATE" = c(state.name, "District of Columbia"),
   "STUSPS" = c(state.abb, "DC"),
-  "division" = factor(c(state.division, "South Atlantic"), levels = levels(state.division)),
-  "region" = factor(c(state.region, "South"), levels = levels(state.region))
+  "division" = factor(
+    c(as.character(state.division), "South Atlantic"),
+    levels = levels(state.division)
+  ),
+  "region" = factor(
+    c(as.character(state.region), "South"),
+    levels = levels(state.region)
+  )
 )
 
 usethis::use_data(usa_states, overwrite = TRUE)
